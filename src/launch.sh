9e875abe5c61bf998e4f28a0c9c3f73394d203c3
Christoph Spörk <christoph.spoerk@gmail.com>
2025-04-01 12:11:01 +0200

    fix layout placing

--- src/launch.sh
+++ src/launch.sh
@@ -10,17 +10,7 @@ sleep 2
 ARGS="--new-window --disable-application-cache --disk-cache-size=0 --disable-features=Translate"
 
 # Get the size of the array
-num_urls=${#urls[@]}
 num_layouts=${#layouts[@]}
-sum_layouts=0
-
-for i in "${layouts_slots[@]}"; do
-  sum_layouts=$((sum_layouts + i))
-done
-TARGET_WORKSPACES=$((num_layouts + (((num_urls - sum_layouts) + 3) / 4) ))
-echo "num urls = $num_urls"
-echo "sum_layouts = $sum_layouts"
-echo "TARGET WORKSPACES = $TARGET_WORKSPACES"
 
 # kill all workspaces
 #!/bin/bash
@@ -36,11 +26,12 @@ echo "Hide cursor"
 i3-msg --socket $(ls /run/user/$(id -u)/i3/ipc-socket.*) "exec unclutter -idle 0"
 echo "Launching chromium"
 # define layout for all workspaces
-for i in $(seq 1 $TARGET_WORKSPACES);
+echo "LAYOUTS: $num_layouts"
+for i in $(seq 1 "$num_layouts");
 do
     i3-msg --socket $(ls /run/user/$(id -u)/i3/ipc-socket.*) "workspace $i;"
     sleep 1
-    echo "Adding layout ${layouts[$i]:-4}"
+    echo "Adding layout ${layouts[$((i-1))]:-4}"
     i3-msg --socket $(ls /run/user/$(id -u)/i3/ipc-socket.*) "append_layout /home/pi/.config/i3/layouts/${layouts[$((i-1))]:-4}.json"
     sleep $DELAY
 done
